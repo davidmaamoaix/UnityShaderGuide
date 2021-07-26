c010e0c1b74140c685a13788281e490c7fd50d91
BuildTools <davidmaamoaix@gmail.com>
2021-07-26 19:12:39 +0800

    more stuff

--- Assets/2.0 - Compute Shaders Go Brrr/RenderTextureScript.cs
+++ Assets/2.0 - Compute Shaders Go Brrr/RenderTextureScript.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RenderTextureScript: MonoBehaviour {
-    
-    void Start() {
-        
+
+    [SerializeField]
+    private ComputeShader _shader;
+    [SerializeField]
+    private int _size = 256;
+
+    private Renderer _renderer;
+    private RenderTexture _texture;
+
+    void Awake() {
+        _texture = new RenderTexture(_size, _size, 24); // 24 for depth of RGBA
+
+        /*
+            Required in order to provide random access and to pass texture
+            from the shader to this texture.          
+        */      
+        _texture.enableRandomWrite = true;
+        _texture.Create();
+
+        _renderer = GetComponent<Renderer>();
     }
 
     void Update() {
