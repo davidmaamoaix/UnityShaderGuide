934ad6c5890d5304e20b4d212e977b5fa259f3b5
BuildTools <davidmaamoaix@gmail.com>
2021-07-27 12:22:58 +0800

    code and expalnation

--- Assets/2.0 - Compute Shaders Go Brrr/RenderTextureScript.cs
+++ Assets/2.0 - Compute Shaders Go Brrr/RenderTextureScript.cs
@@ -8,9 +8,23 @@ public class RenderTextureScript: MonoBehaviour {
     private ComputeShader _shader;
     [SerializeField]
     private int _size = 256;
+    [SerializeField]
+    private int _particleCount = 64;
 
     private Renderer _renderer;
     private RenderTexture _texture;
+    private ComputeBuffer _buffer;
+
+    /*
+        Defines an C# equivalent representation of the particles for 
+        initialization. The struct is memory-copied into its counterpart in the
+        shader code, making the specific type of members not as important as
+        long as the structs share an equivalent memory representation.      
+    */
+    struct Particle {
+        Vector2 pos;
+        Vector2 dir;
+    }
 
     void Awake() {
         _texture = new RenderTexture(_size, _size, 24); // 24 for depth of RGBA
@@ -23,7 +37,23 @@ public class RenderTextureScript: MonoBehaviour {
         _texture.Create();
 
         _renderer = GetComponent<Renderer>();
-    }
+        _renderer.material.SetTexture("_MainTex", _texture);
+
+        /*
+            Round up to nearest multiple of 16, which is the thread count for
+            out compute shader. This circumvent the need for boundary check when
+            referencing the buffer from the shader code.
+        */
+        if (_particleCount % 16 != 0) {
+            _particleCount = _particleCount / 16 + 1;
+        }
+
+        _buffer = new ComputeBuffer(
+            _particleCount,
+            sizeof(float) * 4,
+            ComputeBufferType.Default
+        );
+        }
 
     void Update() {
         int kernel = _shader.FindKernel("ClearBoard");
@@ -31,6 +61,11 @@ public class RenderTextureScript: MonoBehaviour {
         _shader.SetTexture(kernel, "Result", _texture);
         _shader.Dispatch(kernel, _size / 8, _size / 8, 1);
 
-        _renderer.material.SetTexture("_MainTex", _texture);
+
+    }
+
+    void OnDestroy() {
+        _texture.Release();
+        _buffer.Release();
     }
 }
