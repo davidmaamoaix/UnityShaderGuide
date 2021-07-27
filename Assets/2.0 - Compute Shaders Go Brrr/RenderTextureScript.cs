545a0eb3338049cd276225f0f3d1c1cea8c486a4
BuildTools <davidmaamoaix@gmail.com>
2021-07-27 16:13:10 +0800

    finished 2.0 code

--- Assets/2.0 - Compute Shaders Go Brrr/RenderTextureScript.cs
+++ Assets/2.0 - Compute Shaders Go Brrr/RenderTextureScript.cs
@@ -7,13 +7,16 @@ public class RenderTextureScript: MonoBehaviour {
     [SerializeField]
     private ComputeShader _shader;
     [SerializeField]
-    private int _size = 256;
+    private int _size = 128;
     [SerializeField]
     private int _particleCount = 64;
 
     private Renderer _renderer;
     private RenderTexture _texture;
     private ComputeBuffer _buffer;
+
+    private int _kernelClear;
+    private int _kernelParticle;
 
     /*
         Defines an C# equivalent representation of the particles for 
@@ -22,18 +25,19 @@ public class RenderTextureScript: MonoBehaviour {
         long as the structs share an equivalent memory representation.      
     */
     struct Particle {
-        Vector2 pos;
-        Vector2 dir;
+        public Vector2 pos;
+        public Vector2 dir;
     }
 
     void Awake() {
-        _texture = new RenderTexture(_size, _size, 24); // 24 for depth of RGBA
+        _texture = new RenderTexture(_size, _size, 24) { // 24 for depth of RGBA
 
-        /*
-            Required in order to provide random access and to pass texture
-            from the shader to this texture.          
-        */      
-        _texture.enableRandomWrite = true;
+            /*
+                Required in order to provide random access and to pass texture
+                from the shader to this texture.          
+            */
+            enableRandomWrite = true
+        };
         _texture.Create();
 
         _renderer = GetComponent<Renderer>();
@@ -48,24 +52,95 @@ public class RenderTextureScript: MonoBehaviour {
             _particleCount = _particleCount / 16 + 1;
         }
 
+        /*
+            Initializing the buffer for the shader.
+        */
         _buffer = new ComputeBuffer(
             _particleCount,
             sizeof(float) * 4,
             ComputeBufferType.Default
         );
-        }
 
-    void Update() {
-        int kernel = _shader.FindKernel("ClearBoard");
+        /*
+            Get kernel ID from shader. Each kernel ID corresponds to a shader
+            function.          
+        */
+        _kernelClear = _shader.FindKernel("ClearBoard");
+        _kernelParticle = _shader.FindKernel("RunParticle");
 
-        _shader.SetTexture(kernel, "Result", _texture);
-        _shader.Dispatch(kernel, _size / 8, _size / 8, 1);
+        Init();
+    }
 
+    void Update() { 
 
+        /*
+            Updates the global shader variable 'DeltaTime' as it is different
+            every frame.          
+        */
+        _shader.SetFloat("DeltaTime", Time.deltaTime);
+
+        /*
+            Dispatches the product of the parameters thread group, each offset
+            by the geometric grid location.          
+        */
+        _shader.Dispatch(_kernelClear, _size / 8, _size / 8, 1);
+
+        /*
+            Same as above. Note the single dimension of this dispatch statement.
+        */
+        _shader.Dispatch(_kernelParticle, _particleCount / 16, 1, 1);
     }
 
     void OnDestroy() {
+
+        /*
+            Releases the allocated buffer and texture.
+        */
         _texture.Release();
         _buffer.Release();
+    }
+
+    /*
+        Initializes the particles and stores them in the buffer.
+    */
+    private void Init() {
+
+        /*
+            Creates an array object as the content of the buffer.
+        */
+        Particle[] particles = new Particle[_particleCount];
+
+        for (int i = 0; i < particles.Length; i++) {
+
+            /*
+                Assigns each particle with an initial velocity and position.
+            */
+            float maxSpeed = _size * 0.2F;
+
+            Particle particle = new Particle() {
+                pos = new Vector2(
+                    Random.Range(0, _size),
+                    Random.Range(0, _size)
+                ),
+                dir = new Vector2(
+                    Random.Range(-maxSpeed, maxSpeed),
+                    Random.Range(-maxSpeed, maxSpeed)
+                )
+            };
+
+            particles[i] = particle;
+        }
+
+        /*
+            Some global attributes are consistent throughout the entire lifetime
+            of the shader, so we are setting them once here.          
+        */
+        _buffer.SetData(particles);
+
+        _shader.SetInt("BoardSize", _size);
+        _shader.SetTexture(_kernelClear, "Result", _texture);
+
+        _shader.SetTexture(_kernelParticle, "Result", _texture);
+        _shader.SetBuffer(_kernelParticle, "ParticleBuffer", _buffer);
     }
 }
